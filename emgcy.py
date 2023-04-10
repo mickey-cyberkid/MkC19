9e2dda537bb81ebc4d8c4aa6234a4882375ad215
Mickey_Cyberkid <63485016+mickey-cyberkid@users.noreply.github.com>
2023-04-10 22:45:05 +0000

    Update emgcy.py

--- emgcy.py
+++ emgcy.py
@@ -6,5 +6,10 @@ def exec(cmd):
     proc = subprocess.getoutput(cmd)
     print(proc)
     
- cmd = sys.argv[1]
- exec(cmd)
+cmd = sys.argv[1]
+chck_cmd = cmd.lower().split(" ",1)
+if chck_cmd[0] == "cd":
+   os.chdir(chck_cmd[1])
+   print(os.getcwd())
+else :
+   exec(cmd)
